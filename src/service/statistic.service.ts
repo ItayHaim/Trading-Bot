3b22a1bd03e0df4259b302633431ac764e7f3edc
ItayHaim <itayh20@hilma.tech>
2023-08-22 17:16:08 +0300

    working saving statistic

--- src/service/statistic.service.ts
+++ src/service/statistic.service.ts
@@ -1,20 +1,13 @@
-import { writeFile } from 'fs/promises';
 import { resolve } from 'path';
-import statistic from '../statistic.json';
 import { AppDataSource } from '../data-source';
 import { Statistic } from '../entity/Statistic';
-import { MoreThanOrEqual } from 'typeorm';
 
 export class StatisticService {
     private filePath = resolve('src/statistic.json')
 
     async addSuccess() {
         try {
-            await AppDataSource.manager.increment(Statistic, undefined, 'success', 1);
-            // .createQueryBuilder()
-            // .update(Statistic)
-            // .set({ success: () => "success + 1" })
-            // .execute();
+            await AppDataSource.manager.increment(Statistic, {}, 'success', 1);
         } catch (err) {
             console.error('Error writing to file: ' + err)
             throw err
@@ -23,8 +16,7 @@ export class StatisticService {
 
     async addFailed() {
         try {
-            statistic.failed++;
-            await writeFile(this.filePath, JSON.stringify(statistic), 'utf-8');
+            await AppDataSource.manager.increment(Statistic, {}, 'failed', 1);
         } catch (err) {
             console.error('Error writing to file: ' + err)
             throw err
